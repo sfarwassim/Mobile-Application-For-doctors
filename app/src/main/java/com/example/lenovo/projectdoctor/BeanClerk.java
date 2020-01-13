b6e34df35cbd9d65204fff5057fdaf26b8cb4837
Wassim Sfar <wassim.sfar11@gmail.com>
2020-01-13 16:30:17 +0100

    Android Doctor App

---
+++ app/src/main/java/com/example/lenovo/projectdoctor/BeanClerk.java
@@ -0,0 +1,60 @@
+package com.example.lenovo.projectdoctor;
+
+
+
+public class BeanClerk {
+    String name;
+    String id;
+    byte [] ClerkPic;
+    String password;
+    String email;
+    String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public byte[] getClerkPic() {
+        return ClerkPic;
+    }
+
+    public void setClerkPic(byte[] clerkPic) {
+        ClerkPic = clerkPic;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/projectdoctor/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.lenovo.projectdoctor;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lenovo.projectdoctor.viewpager.CustomPager;
+
+public class SplashActivity extends AppCompatActivity {
+    SharedPreferences sp;
+    SharedPreferences.Editor se;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        sp=getSharedPreferences("logininfo",MODE_PRIVATE);
+        se=sp.edit();
+        se.putString("name","sfar");
+        se.putString("pass","1234");
+        se.commit();
+
+        Handler h=new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,CustomPager.class);
+                startActivity(i);
+
+            }
+        },3000);
+    }
+
+}
